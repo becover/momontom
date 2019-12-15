5cd8380bc6738705f1309b888af857406d694d50
becover <sedi.september@gmail.com>
2019-12-16 00:52:05 +0900

    info 기능 구현 완료

--- src/js/info.js
+++ src/js/info.js
@@ -1,15 +1,23 @@
 const buttons = todoList.querySelectorAll('li button');
-console.log(buttons)
+const inputCheckbox = todoList.querySelectorAll('li .btn__done');
+
 function countTodoList() {
   const length = todos.length;
-  console.log(length);
+  const done = todos.filter(todo => todo.checked === true).length;
+  console.log(done)
   infoTotal.innerText = length;
+  infoDone.innerText = done;
+  infoPandding.innerText = length - done;
+  infoAchivement.innerText = `${(done / length * 100).toFixed(1)}%`;
 }
 
 function init() {
   window.addEventListener('load', countTodoList);
   todoForm.addEventListener('submit', countTodoList);
   Array.from(buttons).forEach(button => button.addEventListener('click', countTodoList));
+  todo
+  Array.from(inputCheckbox).forEach(input => input.addEventListener('click', countTodoList));
+  todo
 }
 
 init();
